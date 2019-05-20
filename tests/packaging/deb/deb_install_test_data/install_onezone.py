adaa8d046f15c3b426e334fa52656337ccf71a62
Wojciech Geisler <wojciech.geisler@gmail.com>
2019-05-20 14:37:39 +0200

    VFS-5339 Change emergency username to "onepanel"

--- tests/packaging/deb/deb_install_test_data/install_onezone.py
+++ tests/packaging/deb/deb_install_test_data/install_onezone.py
@@ -7,6 +7,7 @@ from subprocess import STDOUT, check_call, check_output
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
+EMERGENCY_USERNAME = 'onepanel'
 EMERGENCY_PASSPHRASE = 'passphrase'
 
 # get packages
@@ -56,7 +57,7 @@ with open('/root/data/config.yml', 'r') as f:
 
     r = requests.post(
         'https://127.0.0.1:9443/api/v3/onepanel/zone/configuration',
-        auth=('admin', EMERGENCY_PASSPHRASE),
+        auth=(EMERGENCY_USERNAME, EMERGENCY_PASSPHRASE),
         headers={'content-type': 'application/x-yaml'},
         data=f.read(),
         verify=False)
@@ -65,7 +66,7 @@ with open('/root/data/config.yml', 'r') as f:
     status = 'running'
     while status == 'running':
         r = requests.get('https://127.0.0.1:9443' + loc,
-                         auth=('admin', EMERGENCY_PASSPHRASE),
+                         auth=(EMERGENCY_USERNAME, EMERGENCY_PASSPHRASE),
                          verify=False)
         print(r.text)
         assert r.status_code == 200
@@ -83,7 +84,7 @@ for service in ['workers', 'managers', 'databases']:
     r = requests.patch(
         'https://127.0.0.1:9443/api/v3/onepanel/zone/{0}?started=false'.format(
             service),
-        auth=('admin', EMERGENCY_PASSPHRASE),
+        auth=(EMERGENCY_USERNAME, EMERGENCY_PASSPHRASE),
         headers={'content-type': 'application/json'},
         verify=False)
     assert r.status_code == 204
