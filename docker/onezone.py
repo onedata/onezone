26f7015910633cc6b9d71aa05782fbabf46fa607
Darin Nikolow <darnik22@gmail.com>
2023-01-27 07:21:26 +0000

    VFS-9622 Misc focal related fixes

--- docker/onezone.py
+++ docker/onezone.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 import json
@@ -283,7 +283,7 @@ def format_error(response):
 
 def format_dict(details, indent):
     result = ''
-    for key, value in details.items():
+    for key, value in list(details.items()):
         if isinstance(value, dict):
             result += indent + '{}:\n'.format(key)
             result += format_dict(value, indent + '  ')
@@ -349,7 +349,7 @@ def show_ip_address(json):
     ip = '-'
     try:
         ip = sp.check_output(['hostname', '-i']).rstrip('\n')
-        ip = json['NetworkSettings']['Networks'].items()[0][1]['IPAddress']
+        ip = list(json['NetworkSettings']['Networks'].items())[0][1]['IPAddress']
     except Exception:
         pass
     log('* IP Address: {0}'.format(ip))
